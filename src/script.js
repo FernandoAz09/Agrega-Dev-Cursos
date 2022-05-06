701ada417c85cd79b869738d1b24512854292575
Fernando <fsazevedo09@gmail.com>
2022-05-06 13:19:06 -0300

    Refatorado lógica modal

--- src/script.js
+++ src/script.js
@@ -1,16 +1,20 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
 
+//  ABRIR MODAL E TROCAR O ID
+
 for (let card of cards) {
-    card.addEventListener("click", function(){
+    card.addEventListener('click', function () {
         const listId = card.getAttribute("id")
         modalOverlay.classList.add('active')
         modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${listId}`
-
     })
 }
 
-document.querySelector(".close-modal").addEventListener('click', function(){
+
+// FECHAR MODAL
+
+document.querySelector(".close-modal").addEventListener('click', function() {
     modalOverlay.classList.remove('active')
-    modalOverlay.querySelector("iframe").src = ""
+    modalOverlay.querySelector('iframe').src = ""
 })
